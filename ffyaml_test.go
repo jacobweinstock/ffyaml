1090468b057a6cc9be3e0888e4e55c0e6383cc16
Jacob Weinstock <jakobweinstock@gmail.com>
2021-04-19 08:26:48 -0600

    initial commit. clone of https://github.com/peterbourgon/ff/tree/main/ffyaml with map[interface{}]interface{} value support
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

---
+++ ffyaml_test.go
@@ -0,0 +1,72 @@
+package ffyaml
+
+import (
+	"testing"
+	"time"
+
+	"github.com/peterbourgon/ff/v3"
+	"github.com/peterbourgon/ff/v3/fftest"
+)
+
+func TestParser(t *testing.T) {
+	t.Parallel()
+
+	for _, testcase := range []struct {
+		name string
+		file string
+		want fftest.Vars
+	}{
+		{
+			name: "empty input",
+			file: "testdata/empty_input.yaml",
+			want: fftest.Vars{},
+		},
+		{
+			name: "basic KV pairs",
+			file: "testdata/basic.yaml",
+			want: fftest.Vars{S: "hello", I: 10, B: true, D: 5 * time.Second, F: 3.14},
+		},
+		{
+			name: "invalid prefix",
+			file: "testdata/invalid_prefix.yaml",
+			want: fftest.Vars{WantParseErrorString: "found character that cannot start any token"},
+		},
+		{
+			name: "no value",
+			file: "testdata/no_value.yaml",
+			want: fftest.Vars{WantParseErrorIs: ff.StringConversionError{}},
+		},
+		{
+			name: "basic arrays",
+			file: "testdata/basic_array.yaml",
+			want: fftest.Vars{S: "c", X: []string{"a", "b", "c"}},
+		},
+		{
+			name: "multiline arrays",
+			file: "testdata/multi_line_array.yaml",
+			want: fftest.Vars{S: "c", X: []string{"d", "e", "f"}},
+		},
+		{
+			name: "line break arrays",
+			file: "testdata/line_break_array.yaml",
+			want: fftest.Vars{X: []string{"first string", "second string", "third"}},
+		},
+		{
+			name: "unquoted strings in arrays",
+			file: "testdata/unquoted_string_array.yaml",
+			want: fftest.Vars{X: []string{"one", "two", "three"}},
+		},
+	} {
+		t.Run(testcase.name, func(t *testing.T) {
+			fs, vars := fftest.Pair()
+			vars.ParseError = ff.Parse(fs, []string{},
+				ff.WithConfigFile(testcase.file),
+				ff.WithConfigFileParser(Parser),
+				ff.WithAllowMissingConfigFile(true),
+			)
+			if err := fftest.Compare(&testcase.want, vars); err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/jacobweinstock/ffyaml
+
+go 1.16
+
+require (
+	github.com/peterbourgon/ff/v3 v3.0.0
+	gopkg.in/yaml.v2 v2.4.0
+)
